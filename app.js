3e82de6229114fdef541ce23d7b40d93985bae80
Benjamin M. Brown <benjaminbb@gmail.com>
2015-08-03 16:42:57 -0500

    [BUG] Remove require user route

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/user');
+
 
 var app = express();
 
