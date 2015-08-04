ea747837e9559a3c33784507c9c191ea8dc30533
Benjamin M. Brown <benjaminbb@gmail.com>
2015-08-04 15:35:35 -0500

    [BUG] adding payments requirement for route

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-
+var payments = require('./routes/payments');
 
 var app = express();
 
